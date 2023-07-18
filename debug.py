62c675ef87bf65af69f26218b2eb80f47887221f
Jessica Giraldo <jessicacgiraldo@gmail.com>
2023-07-17 22:39:35 -0400

    heck yea adoption owner pet classed BOOYAH

---
+++ debug.py
@@ -0,0 +1,17 @@
+import ipdb
+
+from models.Adoption import Adoption
+from models.Pet import Pet
+from models.Owner import Owner
+
+pet1 = Pet("Fido" , 3 , "dog")
+pet2 = Pet("New Jersey", 1 , "hamster")
+pet3 = Pet("Taco", 44, "parrot")
+pet4 = Pet("Dr. Motorcycle", 5, "cat")
+
+
+
+
+
+
+ipdb.set_trace()
---
+++ models/Adoption.py
@@ -0,0 +1,5 @@
+class Adoption:
+    def __init__(self, description,):
+        self.description = description
+
+
