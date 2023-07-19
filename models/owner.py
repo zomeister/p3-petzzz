5e53c07cc706bf2c2e96cd2fe8277b315af95582
Jessica Giraldo <jessicacgiraldo@gmail.com>
2023-07-19 14:06:40 -0400

    README

--- models/owner.py
+++ models/owner.py
@@ -4,7 +4,7 @@ class Owner:
     def __init__ ( self, name , pronouns):
         self.name = name
         self.pronouns = pronouns
-
+        self.pets = []
 
     def adopt(self, pet):
         pass
